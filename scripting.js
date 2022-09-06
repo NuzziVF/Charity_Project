94e58defc513a96b9ea5cd7edcc9f038739107a5
NuzziVF <dylan.mc.cervantes7@gmail.com>
2022-09-06 14:55:31 -0500

    added Javascript for scrolling text, also added main part of html and the styles to go along with it

---
+++ scripting.js
@@ -0,0 +1,15 @@
+var r_text = new Array ();
+r_text[0] = "Donate to L.I.V.E, support the Invertebrates 🐌";
+r_text[1] = "Donate to Oceans Promotion: Expanding the ocean one day at a time. ";
+r_text[2] = "Donate to Help the Straw Hats 🏴‍☠️👒";
+r_text[3] = "Donate to Help Mental Health Awareness for the Minions";
+r_text[4] = "Donate money to humans!";
+
+
+var i = Math.random();
+
+i = 5 * i;
+
+i = Math.floor(i);
+
+document.write(r_text[i]);
